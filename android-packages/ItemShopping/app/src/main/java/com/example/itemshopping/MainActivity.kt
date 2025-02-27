23c302b9890ea88f43202d89dda83781f2fda2fe
hogansung <wrangle1005@gmail.com>
2025-02-27 15:48:38 -0800

    Include question `ItemShopping`

---
+++ android-packages/ItemShopping/app/src/main/java/com/example/itemshopping/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.itemshopping
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.example.itemshopping.ui.theme.ItemShoppingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ItemShoppingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ItemShoppingApp()
+                }
+            }
+        }
+    }
+}
+
+// Import statements assumed
+@Composable
+fun ItemShoppingApp() {
+    // TODO: Create a NavController using rememberNavController().
+    // TODO: Set up a NavHost with two routes: "list" and "detail/{itemId}".
+}
+
+@Composable
+fun ListScreen(navController: NavController) {
+    val items = List(20) { "Item ${it + 1}" }
+    // TODO: Display the items in a LazyColumn.
+    // TODO: When an item is clicked, navigate to "detail/{itemId}" (pass the item's identifier).
+}
+
+@Composable
+fun DetailScreen(itemId: String) {
+    // TODO: Display a header showing "Detail for <itemId>".
+    // TODO: Create a counter state (use rememberSaveable) and an "Increment" button.
+    // TODO: Display the current counter value.
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ItemShoppingTheme {
+        ItemShoppingApp()
+    }
+}
