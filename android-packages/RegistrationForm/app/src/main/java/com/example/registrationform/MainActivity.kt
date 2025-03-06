e58540cfa5d9600b47d85cddd9931a9a23f4930e
Yellow Bear <percussionpeng@gmail.com>
2025-03-05 17:09:08 -0800

    finish

--- android-packages/RegistrationForm/app/src/main/java/com/example/registrationform/MainActivity.kt
+++ android-packages/RegistrationForm/app/src/main/java/com/example/registrationform/MainActivity.kt
@@ -5,14 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.registrationform.ui.theme.RegistrationFormTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,26 +48,101 @@ and on submit, display a success message.
  */
 @Composable
 fun RegistrationForm(modifier: Modifier = Modifier) {
-    // TODO: Create state variables for username, password, confirm password, and referral code.
-    // TODO: Create a state variable for a checkbox that toggles the visibility of the referral code field.
-    // TODO: Create a state variable for submission status.
+    // Create state variables for username, password, confirm password, and referral code.
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var referralCode by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        // TODO: Add OutlinedTextField for username.
-        // TODO: Add OutlinedTextField for password.
-        // TODO: Add OutlinedTextField for confirm password.
+    // Create a state variable for a checkbox that toggles the visibility of the referral code field.
+    var isCheckboxClicked by remember { mutableStateOf(false) }
 
-        // TODO: Add a Row containing a Checkbox and a label "I have a referral code".
+    // Create a state variable for submission status.
+    var isSubmitSuc by remember { mutableStateOf(false) }
 
-        // TODO: If the checkbox is checked, display an OutlinedTextField for the referral code.
+    Column(modifier = Modifier.padding(16.dp, 48.dp, 16.dp, 16.dp)) {
+        // Add OutlinedTextField for username.
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
 
-        // TODO: Add a Submit button that is enabled only if:
+        // Add OutlinedTextField for password.
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+
+        // Add OutlinedTextField for confirm password.
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirm Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+
+        // Add a Row containing a Checkbox and a label "I have a referral code".
+        Row {
+            Checkbox(
+                checked = isCheckboxClicked,
+                onCheckedChange = {isCheckboxClicked = !isCheckboxClicked}
+            )
+            Text(
+                text = "I have a referral code",
+                modifier = Modifier.padding(8.dp, 12.dp)
+                )
+        }
+
+        // If the checkbox is checked, display an OutlinedTextField for the referral code.
+        if (isCheckboxClicked) {
+            OutlinedTextField(
+                value = referralCode,
+                onValueChange = { referralCode = it },
+                label = { Text("ReferralCode") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+            )
+        }
+
+        // Add a Submit button that is enabled only if:
         //       - username is not blank,
         //       - password is not blank,
         //       - password matches confirm password,
         //       - and, if the referral code field is visible, it is not blank.
+        Button(
+            onClick = {isSubmitSuc = true},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(68.dp, 0.dp, 68.dp),
+            enabled = username != "" &&
+                    password != "" &&
+                    password == confirmPassword &&
+                    ((isCheckboxClicked && referralCode != "") || !isCheckboxClicked)
 
-        // TODO: When submitted, display a success message.
+        ) {
+            Text("Submit")
+        }
+
+        // When submitted, display a success message.
+        if (isSubmitSuc) {
+            Text(
+                text = "Submission Successful!",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+
     }
 }
 
