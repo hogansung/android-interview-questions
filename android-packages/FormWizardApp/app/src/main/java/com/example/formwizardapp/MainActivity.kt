36016273c0e904f334688d6f60725a1af1c31db9
Yellow Bear <percussionpeng@gmail.com>
2025-03-15 22:26:37 -0700

    finish Navigation & StepOneScreen

--- android-packages/FormWizardApp/app/src/main/java/com/example/formwizardapp/MainActivity.kt
+++ android-packages/FormWizardApp/app/src/main/java/com/example/formwizardapp/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.formwizardapp.ui.theme.FormWizardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,12 +65,68 @@ data class FormData(
 fun FormWizardApp(modifier: Modifier = Modifier) {
     // TODO: Create a NavController and set up a NavHost with routes for "stepOne", "stepTwo", "stepThree", and "summary".
     // TODO: Maintain a state variable for FormData (and update it between screens).
+    val navController = rememberNavController()
+    val formDataState = rememberSaveable { mutableStateOf(FormData()) }
+    NavHost(
+        navController = navController,
+        startDestination = "stepOne",
+        modifier = modifier
+    ) {
+        composable("stepOne") {
+            StepOneScreen(
+                navController = navController,
+                formData = formDataState.value,
+                updateFormData = { formDataState.value = it }
+            )
+        }
+        composable("stepTwo") {
+            StepTwoScreen(
+                navController = navController,
+                formData = formDataState.value,
+                updateFormData = { formDataState.value = it }
+            )
+        }
+        composable("stepThree") {
+            StepThreeScreen(
+                navController = navController,
+                formData = formDataState.value,
+                updateFormData = { formDataState.value = it }
+            )
+        }
+        composable("summary") {
+            SummaryScreen(formData = formDataState.value)
+        }
+    }
 }
 
 @Composable
 fun StepOneScreen(navController: NavController, formData: FormData, updateFormData: (FormData) -> Unit) {
+    var firstName by remember { mutableStateOf(formData.firstName) }
+    var lastName by remember { mutableStateOf(formData.lastName) }
+
     // TODO: Display input fields for first name and last name.
     // TODO: Provide a "Next" button to navigate to "stepTwo", updating the FormData.
+    Column {
+        OutlinedTextField(
+            value = lastName,
+            onValueChange = { lastName = it },
+            label = { Text("Last Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = firstName,
+            onValueChange = { firstName = it },
+            label = { Text("First Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            updateFormData(formData.copy(firstName = firstName, lastName = lastName))
+            navController.navigate("stepTwo")
+        },
+            modifier = Modifier.align(Alignment.End)) {Text("Next") }
+    }
 }
 
 @Composable
